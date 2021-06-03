c50a66a90f5474887d510766eb4f4b66e4e35842
Zero <node.zerocool@gmail.com>
2021-06-02 23:58:18 -0500

    hash

--- about_hashes.rb
+++ about_hashes.rb
@@ -3,8 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutHashes < Neo::Koan
   def test_creating_hashes
     empty_hash = Hash.new
-    assert_equal __, empty_hash.class
-    assert_equal(__, empty_hash)
+    assert_equal Hash, empty_hash.class
+    assert_equal({}, empty_hash)
     assert_equal __, empty_hash.size
   end
 
