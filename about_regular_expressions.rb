d03a1e4e40b20a694786ad07aaceb3562375aaee
Zero <node.zerocool@gmail.com>
2021-07-22 23:03:07 -0500

    nil

--- about_regular_expressions.rb
+++ about_regular_expressions.rb
@@ -93,7 +93,7 @@ class AboutRegularExpressions < Neo::Koan
 
   def test_slash_a_anchors_to_the_start_of_the_string
     assert_equal "start", "start end"[/\Astart/]
-    assert_equal __, "start end"[/\Aend/]
+    assert_equal nil, "start end"[/\Aend/]
   end
 
   def test_slash_z_anchors_to_the_end_of_the_string
