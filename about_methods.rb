cb96c5e3c2410be506bef34b486bff1e9130276c
Zero <node.zerocool@gmail.com>
2021-08-04 23:57:34 -0500

    array brackets

--- about_methods.rb
+++ about_methods.rb
@@ -62,7 +62,7 @@ class AboutMethods < Neo::Koan
   end
 
   def test_calling_with_variable_arguments
-    assert_equal array, method_with_var_args.class
+    assert_equal [], method_with_var_args.class
     assert_equal __, method_with_var_args
     assert_equal __, method_with_var_args(:one)
     assert_equal __, method_with_var_args(:one, :two)
