d51f4331723cb329826e50ba59111aba453221a9
Zero <node.zerocool@gmail.com>
2021-10-04 23:20:02 -0500

    happy wag

--- about_open_classes.rb
+++ about_open_classes.rb
@@ -23,7 +23,7 @@ class AboutOpenClasses < Neo::Koan
 
   def test_after_reopening_dogs_can_both_wag_and_bark
     fido = Dog.new
-    assert_equal __, fido.wag
+    assert_equal "HAPPY", fido.wag
     assert_equal __, fido.bark
   end
 
