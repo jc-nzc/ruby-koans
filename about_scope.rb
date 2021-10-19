c51c4091d327056603d04d0b31d27983625fca07
Zero <node.zerocool@gmail.com>
2021-10-18 23:30:26 -0500

    identify method

--- about_scope.rb
+++ about_scope.rb
@@ -26,7 +26,7 @@ class AboutScope < Neo::Koan
   def test_you_can_reference_nested_classes_using_the_scope_operator
     fido = Jims::Dog.new
     rover = Joes::Dog.new
-    assert_equal __, fido.identify
+    assert_equal :jims_dog, fido.identify
     assert_equal __, rover.identify
 
     assert_equal __, fido.class != rover.class
