a5fdcd76fc3242e149c18d666b32942c0aba172a
Zero <node.zerocool@gmail.com>
2021-11-15 23:55:23 -0600

    nme

--- about_message_passing.rb
+++ about_message_passing.rb
@@ -151,13 +151,13 @@ class AboutMessagePassing < Neo::Koan
     catcher = WellBehavedFooCatcher.new
 
     assert_equal "Foo to you too", catcher.foo_bar
-    assert_equal __, catcher.foo_baz
+    assert_equal "Foo to you too", catcher.foo_baz
   end
 
   def test_non_foo_messages_are_treated_normally
     catcher = WellBehavedFooCatcher.new
 
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       catcher.normal_undefined_method
     end
   end
