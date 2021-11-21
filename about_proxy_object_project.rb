011f9b16380900f81dc16cc818dae7fe3662d831
Zero <node.zerocool@gmail.com>
2021-11-20 23:04:48 -0600

    missing and respond

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -24,10 +24,24 @@ class Proxy
   def called?(method_name)
     @messages.include?(method_name)
   end
-  
+
   def number_of_times_called(method_name)
     @messages.count(method_name)
   end
+
+  def method_missing(method_name, *args, &block)
+    if(@object.respond_to?(method_name))
+      @messages << method_name
+      @object.send(method_name, *args)
+    else
+      @object.method_missing(method_name, *args, &block)
+    end
+  end
+
+  def respond_to?(method_name)
+    @object.respond_to?(method_name)
+  end
+end
 
   # WRITE CODE HERE
 end
